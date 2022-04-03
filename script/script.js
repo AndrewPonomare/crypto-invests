d6b01da02ed1bfc9229c3d89e95458bb1de83737
Andrii Ponomarenko <Andrewponomare@gmail.com>
2022-04-03 21:24:09 +0300

    fix js

--- script/script.js
+++ script/script.js
@@ -5,11 +5,11 @@ let language = document.getElementById('btn-language')
 let contact  = document.getElementById('contact')
 
 
-language.addEventListener('click', () => {
+language.addEventListener('mouseover', () => {
     main.style.filter = 'blur(5px)'
 })
 
-dropdown.addEventListener('click', () => {
+dropdown.addEventListener('mouseover', () => {
     main.style.display = "none"
     footer.style.display = "none"
     contact.style.display = 'block'
